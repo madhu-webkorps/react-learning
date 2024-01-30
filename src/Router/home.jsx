8f7173f10d1f4a765ef79fa487a12df3a7555351
madhu-webkorps <madhu@webkorps.com>
2024-01-30 15:33:08 +0530

    react router link component

--- src/Router/home.jsx
+++ src/Router/home.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
-import Contact from './contact'
-import About from './about'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home Page</div>
+    <>
+      <h1>Welcome to Home Page</h1>
+      <header>
+        <a href="">LOGO</a>
+        <nav>
+          <li><Link to = '/about'>Home</Link></li>
+          <li><Link to = '/about'>About</Link></li>
+          <li><Link to = '/contact'>Contact</Link></li>
+        </nav>
+      </header>
+
+    </>
   )
 }
 
