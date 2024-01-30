d68d5df5d49b97e57103f874578ae93e141e076a
madhu-webkorps <madhu@webkorps.com>
2024-01-30 12:51:04 +0530

    functions basics

---
+++ src/Calculation.jsx
@@ -0,0 +1,17 @@
+function Addition(a, b){
+    return a + b;
+}
+
+function Subtraction(a, b){
+    return a - b;
+}
+
+function multipication(a, b){
+    return a * b;
+}
+
+function division(a,b){
+    return a / b;
+}
+
+export {Addition, Subtraction, multipication, division};
